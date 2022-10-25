7c12a2d26bff6a4c22456fdb4b71411fae5c0e6b
ShourovHasan <shourovhasan@gmail.com>
2022-10-25 17:08:21 +0600

    firebase hosting deploy and context api added

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,75 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    // console.log(user);
+
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const googleProviderLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const githubProviderLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, githubProvider)
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    const verifyEmail = () => {
+        return sendEmailVerification(auth.currentUser);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setLoading(false);
+            if (currentUser === null || currentUser.emailVerified) {
+                setUser(currentUser);
+            }
+            else {
+                setUser(null);
+            }
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+    const logOut = () => {
+        setLoading(true);
+        setUser(null);
+        return signOut(auth);
+    }
+
+    const authInfo = { user, setUser, googleProviderLogin, githubProviderLogin, logOut, createUser, signIn, loading, updateUserProfile, verifyEmail, setLoading };
+    
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
