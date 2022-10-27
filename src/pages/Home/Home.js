4c56af46e254f3c108493be3404b0a3906df6655
ShourovHasan <shourovhasan@gmail.com>
2022-10-27 18:50:37 +0600

    Home page login and register button added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
@@ -13,7 +13,9 @@ const Home = () => {
                     </Col>
                     <Col lg="5" className='home_container bg-black text-white text-center py-3'>
                         <h3>The online website "AIUB PORTAL" is a great resource for learning courses. It offers a variety of courses that are suitable for all levels of learners. The courses are well organized and easy to follow. The website also provides a community forum where users can interact with each other and share their experiences.</h3>
-                        <Link className='btn btn-primary mt-3 w-50 mb-2 m-auto display-5' to={`/allCourses`}>Get Started</Link>
+                        <Link className='btn btn-primary mt-3 w-50 mb-2 m-auto display-5' to={`/allCourses`}>Get Started</Link> <br></br>
+                        <Link className='btn btn-primary me-2 px-4' to='/login'>Login</Link>
+                        <Link className='btn btn-primary px-4' to='/register'>Register</Link>
                     </Col>
                 </Row>
             </Container>
